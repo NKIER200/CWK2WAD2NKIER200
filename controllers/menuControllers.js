2a02cd3f4acf494d40245ed08fa7ab2c5f86735d
NKIER200 <NKIER200@Caledonian.ac.uk>
2022-05-06 17:34:08 +0100

    make it better5

--- controllers/menuControllers.js
+++ controllers/menuControllers.js
@@ -5,7 +5,7 @@ const db = new menuDAO();
 db.init();
 
 exports.show_login = function (req, res) {
-  res.render("user/login");
+  res.render("../user/login");
 };
 
 exports.handle_login = function (req, res) {
